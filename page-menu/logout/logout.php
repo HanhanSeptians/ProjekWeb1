8a6a7d69286e29d89b54603c23a36b5ab6319926
HanhanSeptians <hanhan.septian.tif20@polban.ac.id>
2022-04-25 21:10:12 +0700

    logout

--- page-menu/logout/logout.php
+++ page-menu/logout/logout.php
@@ -1,5 +1,10 @@
 <?php
 
-    include '../navbar/navbar.php';
-
+session_start();
+session_destroy();
+echo '<script type ="text/JavaScript">';  
+echo 'alert("LOGOUT BERHASIL")';  
+echo '</script>'; 
+echo '<meta http-equiv="refresh" content="0.1;url=../login/login.php">';
+exit;
 ?>
